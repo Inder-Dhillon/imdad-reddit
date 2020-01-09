a6bbd2cfa201f02b2432cd175c3b8d75a1e0f276
Inderpal Singh Dhillon <inder_dhillon@outlook.com>
2020-01-09 18:57:34 -0500

    Does not self-comment now on reddit

--- reddit_bot.py
+++ reddit_bot.py
@@ -7,35 +7,37 @@ reddit = praw.Reddit(client_id=environ['REDDIT_ID'],
                      password=environ['REDDIT_PASSWORD'],
                      user_agent='imdad',
                      username='imdad_bot')
+
 subreddit = reddit.subreddit("all")  # subreddits
 
 for comment in subreddit.stream.comments():
-    comment_lower = comment.body.lower()
-    comment_lower_list = comment_lower.split(" ")
-    try:
-        if "im" in comment_lower_list:
-            trigger_loc = comment_lower.find("im ") + 3
-            end_loc = comment_lower.find(".", trigger_loc)
-            if end_loc == -1:
-                end_loc = comment_lower.find("\n", trigger_loc)
+    if reddit.user.me() != comment.author.name:
+        comment_lower = comment.body.lower()
+        comment_lower_list = comment_lower.split(" ")
+        try:
+            if "im" in comment_lower_list:
+                trigger_loc = comment_lower.find("im ") + 3
+                end_loc = comment_lower.find(".", trigger_loc)
                 if end_loc == -1:
-                    comment.reply("Hi " + comment.body[trigger_loc:]+ ", I'm Dad👨")
+                    end_loc = comment_lower.find("\n", trigger_loc)
+                    if end_loc == -1:
+                        comment.reply("Hi " + comment.body[trigger_loc:]+ ", I'm Dad👨")
+                    else:
+                        comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
                 else:
                     comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
-            else:
-                comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
-        elif "i'm" in comment_lower_list:
-            trigger_loc = comment_lower.find("i'm ") + 4
-            end_loc = comment_lower.find(".", trigger_loc)
-            if end_loc == -1:
-                end_loc = comment_lower.find("\n", trigger_loc)
+            elif "i'm" in comment_lower_list:
+                trigger_loc = comment_lower.find("i'm ") + 4
+                end_loc = comment_lower.find(".", trigger_loc)
                 if end_loc == -1:
-                    comment.reply("Hi " + comment.body[trigger_loc:] + ", I'm Dad👨")
+                    end_loc = comment_lower.find("\n", trigger_loc)
+                    if end_loc == -1:
+                        comment.reply("Hi " + comment.body[trigger_loc:] + ", I'm Dad👨")
+                    else:
+                        comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
                 else:
                     comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
-            else:
-                comment.reply("Hi " + comment.body[trigger_loc:end_loc] + ", I'm Dad👨")
 
-    except:
-        requests.get(url='https://imdadbot.herokuapp.com/')
-        pass
+        except:
+            requests.get(url='https://imdadbot.herokuapp.com/')
+            pass
